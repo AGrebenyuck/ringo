cfb65bafffed22cfa490bea33ba4d7a718344a8b
AGrebenyuck <djtemka0@gmail.com>
2022-02-24 22:30:31 +0200

    adde header, directions, provide

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
 $(function(){
-  
+  const swiper = new Swiper('.swiper', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      renderBullet: function(index, className) {
+        return `<span class="${className}">${index + 1}</span>`
+      },
+      clickable: true
+    },
+    // autoHeight: true,
+    autoplay: {
+      delay: 5000,
+      pauseOnMouseEnter: true,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 })
